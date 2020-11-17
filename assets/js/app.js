4c9d93c22cf3e2e1daeee5e6743f40d0f533cb2d
Alistair <deadlyhardware@gmail.com>
2020-11-17 13:08:51 -0500

    :zap: Work on building out user interface and refining the update function

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,10 +15,16 @@ const showModal = () => {
     const modalName = document.querySelector(".modal-name");
     const modalEmail = document.querySelector(".modal-email");
     const modalClasscode = document.querySelector(".modal-classcode");
+    const modalCourseType = document.querySelector(".modal-course-type");
+    const modalTutorName = document.querySelector(".modal-tutor-name");
+
 
     modalName.textContent = "Name: " + readFromLocalStorage().name;
     modalEmail.textContent = "Email: " + readFromLocalStorage().email;
     modalClasscode.textContent = "Classcode: " + readFromLocalStorage().classcode;
+    modalCourseType.textContent = "Course Type: " + readFromLocalStorage().courseType;
+    modalTutorName.textContent = "Tutor Name: " + readFromLocalStorage().tutorName;
+
 };
 
 const hideModal = () => {
@@ -63,24 +69,36 @@ const getStudentInfo = () => {
     const name = document.querySelector(".name").value.trim();
     const email = document.querySelector(".email").value.trim();
     const classcode = document.querySelector(".classcode").value.trim();
-    const studentObject = new GenerateStudentObject(name, email, classcode);
+    const courseType = document.querySelector(".course-type").value.trim();
+    const tutorName = document.querySelector(".tutor-name").value.trim();
+    const studentObject = new GenerateStudentObject(name, email, classcode, courseType, tutorName);
     saveToLocalStorage(studentObject);
 };
 
 class GenerateStudentObject {
-    constructor(name, email, classcode) {
+    constructor(name, email, classcode, courseType, tutorName) {
         this.name = name;
         this.email = email;
         this.classcode = classcode;
+        this.courseType = courseType;
+        this.tutorName = tutorName;
     }
 }
 
 const fillOutForm = () => {
-    const name = readFromLocalStorage().name
+
+    const fullName = readFromLocalStorage().name
     const email = readFromLocalStorage().email
     const classcode = readFromLocalStorage().classcode
-    // openEvalFormButton.href = `https://docs.google.com/forms/d/e/1FAIpQLSc_q0CSp5Bpn7lfDAdoPCbBTW-OxWQVhC3gG5P9e6iE4FERjw/viewform?&entry.1626809215=${classcode}&entry.1262798942=${name}&entry.1509111758=${email}&entry.737967299=Alistair%20Rowden`
-    openEvalFormButton.href = `eval form link goes here!!`
+    const courseType = "FSF - Full Stack Flex Web Development(Javascript)";
+    const tutorName = readFromLocalStorage().tutorName.split(' ').reverse().join(', ');
+    const year = new Date().getFullYear();
+    const month = new Date().getMonth() + 1;
+    const day = new Date().getDate();
+
+    const date = `&entry.401287639_year=${year}&entry.401287639_month=${month}&entry.401287639_day=${day}`
+    const studentInfo = `?&entry.737967299=${fullName}&entry.760591640=${email}&entry.526528092=${classcode}&entry.698926831=No&entry.831839664=${tutorName}&entry.134267295=${courseType}${date}`;
+    openEvalFormButton.href = `https://docs.google.com/forms/d/e/1FAIpQLSdb4ejjbqoqKO-Q4k7zeO_xwykwB0dxYLWYm1mX5Ik45MzEeg/viewform${studentInfo}`
 }
 
 // If there is data in localStorage generate the eval page else show the home screen.
@@ -95,12 +113,26 @@ saveStudentButton.addEventListener("click", () => {
     getStudentInfo();
     generateEvalPage();
 });
+
 deleteInfo.addEventListener("click", () => {
     deleteFromLocalStorage();
     window.location.reload();
 });
+
 updateInfo.addEventListener("click", () => {
     showHomeScreen();
     hideModal();
+    const name = document.querySelector(".name");
+    const email = document.querySelector(".email");
+    const classcode = document.querySelector(".classcode");
+    const courseType = document.querySelector(".course-type");
+    const tutorName = document.querySelector(".tutor-name");
+
+    name.value = readFromLocalStorage().name;
+    email.value = readFromLocalStorage().email;
+    classcode.value = readFromLocalStorage().classcode;
+    courseType.value = readFromLocalStorage().courseType;
+    tutorName.value = readFromLocalStorage().tutorName;
 });
-openEvalFormButton.addEventListener("click", fillOutForm)
+
+openEvalFormButton.addEventListener("click", fillOutForm);
