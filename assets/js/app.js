f427e4c1e24369f59d90a3bb439c3d858267af1c
Alistair <deadlyhardware@gmail.com>
2020-11-17 23:18:48 -0500

    :tada: Implement new helper function to help streamline the dark mode code (hey that rhymes)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,30 @@
-const profileButton = document.querySelector(".profile-button");
-const infoButton = document.querySelector(".info-button");
-const darkModeButton = document.querySelector(".dark-mode-button");
-const closeModalButton = document.querySelector(".close-button");
-const modal = document.querySelector(".modal");
-const alertModal = document.querySelector(".alert-modal");
-const closeAlertModalButton = document.querySelector(".close-alert-modal-button");
-const yesNoModal = document.querySelector(".yes-no-modal");
-const yesButton = document.querySelector(".yes-button");
-const noButton = document.querySelector(".no-button");
-const updateInfo = document.querySelector(".update-info");
-const deleteInfo = document.querySelector(".delete-info");
-const saveStudentButton = document.querySelector(".save");
-const inputFields = document.querySelector(".input-fields");
-const evalPage = document.querySelector(".eval");
-const openEvalFormButton = document.querySelector(".open-eval-form");
-const courseTypeButton = document.querySelectorAll("#courseTypeButton");
+const $ = document;
+const profileButton = $.querySelector(".profile-button");
+const infoButton = $.querySelector(".info-button");
+const darkModeButton = $.querySelector(".dark-mode-button");
+const closeModalButton = $.querySelector(".close-button");
+const modal = $.querySelector(".modal");
+const alertModal = $.querySelector(".alert-modal");
+const closeAlertModalButton = $.querySelector(".close-alert-modal-button");
+const yesNoModal = $.querySelector(".yes-no-modal");
+const yesButton = $.querySelector(".yes-button");
+const noButton = $.querySelector(".no-button");
+const updateInfo = $.querySelector(".update-info");
+const deleteInfo = $.querySelector(".delete-info");
+const saveStudentButton = $.querySelector(".save");
+const inputFields = $.querySelector(".input-fields");
+const evalPage = $.querySelector(".eval");
+const openEvalFormButton = $.querySelector(".open-eval-form");
+const courseTypeButton = $.querySelectorAll("#courseTypeButton");
 
 
 const showModal = () => {
     modal.classList.add("is-active");
-    const modalFullName = document.querySelector(".modal-full-name");
-    const modalEmail = document.querySelector(".modal-email");
-    const modalClasscode = document.querySelector(".modal-classcode");
-    const modalCourseType = document.querySelector(".modal-course-type");
-    const modalTutorName = document.querySelector(".modal-tutor-name");
+    const modalFullName = $.querySelector(".modal-full-name");
+    const modalEmail = $.querySelector(".modal-email");
+    const modalClasscode = $.querySelector(".modal-classcode");
+    const modalCourseType = $.querySelector(".modal-course-type");
+    const modalTutorName = $.querySelector(".modal-tutor-name");
 
     modalFullName.textContent = "Full Name: " + readFromLocalStorage().fullName;
     modalEmail.textContent = "Email: " + readFromLocalStorage().email;
@@ -74,7 +75,7 @@ const generateEvalPage = () => {
         evalPage.style.display = "block";
     }
 
-    const welcomeMessage = document.querySelector('.welcome-message');
+    const welcomeMessage = $.querySelector('.welcome-message');
     const firstName = readFromLocalStorage().fullName.split(' ')[0];
     welcomeMessage.textContent = `👋 Welcome back, ${firstName}!`
 };
@@ -87,11 +88,11 @@ const showHomeScreen = () => {
 };
 
 const getStudentInfo = () => {
-    const fullName = document.querySelector(".full-name").value.trim();
-    const email = document.querySelector(".email").value.trim();
-    const classcode = document.querySelector(".classcode").value.trim();
-    const courseType = document.querySelector(".course-type").value.trim();
-    const tutorName = document.querySelector(".tutor-name").value.trim();
+    const fullName = $.querySelector(".full-name").value.trim();
+    const email = $.querySelector(".email").value.trim();
+    const classcode = $.querySelector(".classcode").value.trim();
+    const courseType = $.querySelector(".course-type").value.trim();
+    const tutorName = $.querySelector(".tutor-name").value.trim();
 
     const studentObject = new GenerateStudentObject(fullName, email, classcode, courseType, tutorName);
     saveToLocalStorage(studentObject);
@@ -112,8 +113,8 @@ const fillOutForm = () => {
     const fullName = readFromLocalStorage().fullName
     const email = readFromLocalStorage().email
     const classcode = readFromLocalStorage().classcode
-    const courseType = "FSF - Full Stack Flex Web Development(Javascript)";
-    const tutorName = readFromLocalStorage().tutorName.split(' ').reverse().join(', ');
+    const courseType = readFromLocalStorage().courseType;
+    const tutorName = readFromLocalStorage().tutorName;
     const year = new Date().getFullYear();
     const month = new Date().getMonth() + 1;
     const day = new Date().getDate();
@@ -122,46 +123,41 @@ const fillOutForm = () => {
     const studentInfo = `?&entry.737967299=${fullName}&entry.760591640=${email}&entry.526528092=${classcode}&entry.698926831=No&entry.831839664=${tutorName}&entry.134267295=${courseType}${date}`;
     openEvalFormButton.href = `https://docs.google.com/forms/d/e/1FAIpQLSdb4ejjbqoqKO-Q4k7zeO_xwykwB0dxYLWYm1mX5Ik45MzEeg/viewform${studentInfo}`
 }
+// helper function to help with the dark mode function below
+const darkModeHelper = (element, className) => {
+    element.forEach(x=>x.classList.toggle(className));
+}
 
 const toggleDarkMode = () => {
-    const html = document.querySelector('html');
-    const navbar = document.querySelector('.navbar');
-    const button = document.querySelectorAll('.button');
-    const input = document.querySelectorAll('.input');
-    const modalHeader = document.querySelectorAll('.modal-card-head');
-    const modalBody = document.querySelectorAll('.modal-card-body');
-    const modalFooter = document.querySelectorAll('.modal-card-foot');
-    const hr = document.querySelectorAll('hr');
-    const p = document.querySelectorAll('p');
-    const footer = document.querySelector('.footer');
+    const html = $.querySelector('html');
+    const navbar = $.querySelector('.navbar');
+    const button = $.querySelectorAll('.button');
+    const input = $.querySelectorAll('.input');
+    const modalHeader = $.querySelectorAll('.modal-card-head');
+    const modalBody = $.querySelectorAll('.modal-card-body');
+    const modalFooter = $.querySelectorAll('.modal-card-foot');
+    const hr = $.querySelectorAll('hr');
+    const p = $.querySelectorAll('p');
+    const footer = $.querySelector('.footer');
 
     html.classList.toggle('dark-mode-background-is-active');
     navbar.classList.toggle('dark-mode-background-is-active');
     footer.classList.toggle('dark-mode-background-is-active');
-    button.forEach(x=>x.classList.toggle('is-dark'));
 
-    input.forEach(x=>x.classList.toggle('dark-mode-is-active-offset'));
-    input.forEach(x=>x.classList.toggle('dark-mode-text-is-active'));
-
-    modalHeader.forEach(x=>x.classList.toggle('dark-mode-is-active-offset'));
-    modalHeader.forEach(x=>x.classList.toggle('dark-mode-text-is-active'));
-
-    modalBody.forEach(x=>x.classList.toggle('dark-mode-is-active-offset'));
-    modalBody.forEach(x=>x.classList.toggle('dark-mode-text-is-active'));
-
-    modalFooter.forEach(x=>x.classList.toggle('dark-mode-is-active-offset'));
-    modalFooter.forEach(x=>x.classList.toggle('dark-mode-text-is-active'));
-
-    hr.forEach(x=>x.classList.toggle('dark-mode-background-is-active'));
-    hr.forEach(x=>x.classList.toggle('dark-mode-text-is-active'));
-
-    p.forEach(x=>x.classList.toggle('dark-mode-text-is-active'));
-
-
+    darkModeHelper(button, 'is-dark');
+    darkModeHelper(input, 'dark-mode-is-active-offset');
+    darkModeHelper(input, 'dark-mode-text-is-active');
+    darkModeHelper(modalHeader, 'dark-mode-is-active-offset');
+    darkModeHelper(modalHeader, 'dark-mode-text-is-active');
+    darkModeHelper(modalBody, 'dark-mode-is-active-offset');
+    darkModeHelper(modalBody, 'dark-mode-text-is-active');
+    darkModeHelper(modalFooter, 'dark-mode-is-active-offset');
+    darkModeHelper(modalFooter, 'dark-mode-text-is-active');
+    darkModeHelper(hr, 'dark-mode-background-is-active');
+    darkModeHelper(hr, 'dark-mode-text-is-active');
+    darkModeHelper(p, 'dark-mode-text-is-active');
 }
 
-// If there is data in localStorage generate the eval page else show the home screen.
-readFromLocalStorage() ? generateEvalPage() : showHomeScreen();
 
 profileButton.addEventListener("click", () => {
     readFromLocalStorage() ? showModal() : showAlertModal();
@@ -178,11 +174,11 @@ deleteInfo.addEventListener("click", () => {
 });
 
 yesButton.addEventListener("click", () => {
-    const fullName = document.querySelector(".full-name");
-    const email = document.querySelector(".email");
-    const classcode = document.querySelector(".classcode");
-    const courseType = document.querySelector(".course-type");
-    const tutorName = document.querySelector(".tutor-name");
+    const fullName = $.querySelector(".full-name");
+    const email = $.querySelector(".email");
+    const classcode = $.querySelector(".classcode");
+    const courseType = $.querySelector(".course-type");
+    const tutorName = $.querySelector(".tutor-name");
 
     fullName.value = '';
     email.value = '';
@@ -200,11 +196,11 @@ noButton.addEventListener("click", hideYesNoModal)
 updateInfo.addEventListener("click", () => {
     showHomeScreen();
     hideModal();
-    const fullName = document.querySelector(".full-name");
-    const email = document.querySelector(".email");
-    const classcode = document.querySelector(".classcode");
-    const courseType = document.querySelector(".course-type");
-    const tutorName = document.querySelector(".tutor-name");
+    const fullName = $.querySelector(".full-name");
+    const email = $.querySelector(".email");
+    const classcode = $.querySelector(".classcode");
+    const courseType = $.querySelector(".course-type");
+    const tutorName = $.querySelector(".tutor-name");
 
     fullName.value = readFromLocalStorage().fullName;
     email.value = readFromLocalStorage().email;
@@ -217,7 +213,7 @@ openEvalFormButton.addEventListener("click", fillOutForm);
 closeAlertModalButton.addEventListener("click", hideAlertModal);
 darkModeButton.addEventListener("click", () => {
     toggleDarkMode();
-    const moonIcon = document.querySelector('.fa-moon');
+    const moonIcon = $.querySelector('.fa-moon');
     moonIcon.classList.toggle('rotateIn');
     const isDarkMode = JSON.parse(localStorage.getItem('is-dark-mode'));
     isDarkMode ? localStorage.setItem('is-dark-mode', false) : localStorage.setItem('is-dark-mode', true);  
@@ -225,3 +221,6 @@ darkModeButton.addEventListener("click", () => {
 
 const isDarkMode = JSON.parse(localStorage.getItem('is-dark-mode'));
 isDarkMode ? toggleDarkMode() : null;
+
+// If there is data in localStorage generate the eval page else show the home screen.
+readFromLocalStorage() ? generateEvalPage() : showHomeScreen();
